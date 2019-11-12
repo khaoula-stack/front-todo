31c7fe50cae49b4e464ca4c11720acbb55619153
Khaoula <achour.khaoula1@gmail.com>
2019-11-11 14:05:09 -1200

    fourthcomit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(registerBuilder:FormBuilder) { 
+    this.registerForm= registerBuilder.group({
+      firstname: new FormControl(),
+      lastname: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      password: new FormControl(),
+      repassword: new FormControl()
+    })
+    
+  }
 
   ngOnInit() {
+  }
+  register() {
+    console.log(this.registerForm.value)
   }
 
 }
